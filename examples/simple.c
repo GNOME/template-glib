a8d113b7d8dcc0ff5333f9133cd131b3c3a20c0c
Christian Hergert <chergert@redhat.com>
2016-01-11 04:43:53 -0800

    examples: add some comments

--- examples/simple.c
+++ examples/simple.c
@@ -14,6 +14,11 @@ main (gint   argc,
   const gchar *output;
   gchar zero = 0;
 
+  /*
+   * First we need to create and parse our template.
+   * The template can be expanded multiple times, so you can
+   * keep them around and re-use them.
+   */
   file = g_file_new_for_path ("simple.tmpl");
   tmpl = tmpl_template_new (NULL);
 
@@ -23,24 +28,42 @@ main (gint   argc,
       return EXIT_FAILURE;
     }
 
+  /*
+   * Now create our scope used to expand the template,
+   * and assign the "title" variable in the scope.
+   */
   scope = tmpl_scope_new (NULL);
 
   symbol = tmpl_scope_get (scope, "title");
   tmpl_symbol_assign_string (symbol, "My Title");
 
-  /* We could use a unix output stream, or socket stream, as well */
+  /*
+   * Now lets expand the template into a memory stream. We could also
+   * just use a unix output stream using STDOUT, but this is more portable
+   * if people want to get this running on non-UNIX systems.
+   */
   stream = g_memory_output_stream_new (NULL, 0, g_realloc, g_free);
-
   if (!tmpl_template_expand (tmpl, stream, scope, NULL, &error))
     {
       g_printerr ("%s\n", error->message);
       return EXIT_FAILURE;
     }
 
-  /* add trailing \0, not necessary if we weren't printing with g_print */
+  /*
+   * Because we are converting this to a C String, we need to add a trailing
+   * null byte.
+   */
   g_output_stream_write (stream, &zero, 1, NULL, NULL);
+
+  /*
+   * Okay, finally we can print this to stdout.
+   */
   output = g_memory_output_stream_get_data (G_MEMORY_OUTPUT_STREAM (stream));
   g_print ("%s\n", output);
+
+  /*
+   * All our state gets cleaned up thanks to g_autoptr()!
+   */
 
   return EXIT_SUCCESS;
 }
